05159118e9e7477a11617f14ce429c86eccfa854
akashsingh_bhandari <asbhandari14@gmail.com>
2024-04-21 10:17:16 +0530

    Changing the Navbar Title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ export default function Navbar(props) {
       </div> */}
       <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
   <input className="form-check-input" onClick = {props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
-  <label className="form-check-label " htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+  <label className="form-check-label " htmlFor="flexSwitchCheckDefault">{props.mode==="light"?"Enable DarkMode" : "Enable LightMode"}</label>
 </div>
     </div>
   </div>
